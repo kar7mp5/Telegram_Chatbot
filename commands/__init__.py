569d71e41309397cb114f0f531d2545d7579b625
kar7mp5 <tommy1005a@gmail.com>
2024-10-14 01:16:32 +0900

    Add the help.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,6 +4,7 @@ from sys import path
 
 path.insert(0, dirname(__file__))
 
+from .help import help
 from .start import start
 from .unknown import unknown
 from .inline_query import advice, inline_query
---
+++ commands/help.py
@@ -0,0 +1,24 @@
+# help.py
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+description = """\
+`/help` - Show all commands
+`@bot weather <city>` - Show weather forecast for the specified city
+"""
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handles help commands.
+    
+    Explain about telegram bot's handlers.
+
+    Args:
+        update (Update): Incoming update containing the message.
+        context (ContextTypes.DEFAULT_TYPE): Contextual information related to the command.
+    """
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=description
+    )
