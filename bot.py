2922e5920ac7f61c6274621797cf75cc98635d1e
kar7mp5 <tommy1005a@gmail.com>
2024-10-19 15:40:08 +0900

    Delete inline-query funcs

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 #! ./venv/bin/python
-# bot.py
+
+"""
+bot.py
+
+We are trying to create a bot that can communicate through the LLM model.
+The ability to give commands through `/` in Telegram and chat with bots in general conversations.
+"""
+
 import logging
-from telegram.ext import filters, ApplicationBuilder, CommandHandler, InlineQueryHandler, MessageHandler, ContextTypes
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler
 
 # Load bot commands
 import commands
@@ -11,21 +18,23 @@ import commands
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN")
-
-
-# Set up the log style
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 
 
 
+def main():
+    # Set up the log style
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
 
-if __name__ == '__main__':
+
+    # Load telegram token
+    load_dotenv()
+    TOKEN = os.getenv("BOT_TOKEN")
+
     application = ApplicationBuilder().token(TOKEN).build()
+
 
     # Handlers
     help_handler = CommandHandler('help', commands.help)
@@ -33,14 +42,18 @@ if __name__ == '__main__':
 
     weather_handler = CommandHandler('weather', commands.weather)
     application.add_handler(weather_handler)
-
-    # inlinequery_handler = InlineQueryHandler(commands.inline_query)
-    # application.add_handler(inlinequery_handler)
     
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, commands.gpt_response))
+
 
     # Other handlers
     unknown_handler = MessageHandler(filters.COMMAND, commands.unknown)
     application.add_handler(unknown_handler)
 
     application.run_polling()
+
+
+
+
+if __name__ == '__main__':
+    main()
--- commands/start.py
+++
@@ -1,9 +0,0 @@
-# start.py
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import filters, ApplicationBuilder, MessageHandler, ContextTypes, CommandHandler, InlineQueryHandler
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!"
-    )
