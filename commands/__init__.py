f92a1559e37d34e753f48b9cb21d5e23b90c79f6
kar7mp5 <tommy1005a@gmail.com>
2024-10-20 01:04:15 +0900

    Merge the hotfix2

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,7 +2,9 @@
 from os.path import dirname
 from sys import path
 
+
 path.insert(0, dirname(__file__))
+
 
 from .start import start
 from .help import help
--- commands/start.py
+++ commands/start.py
@@ -4,6 +4,12 @@ from telegram.ext import ContextTypes
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Start the bot
+
+    Args:
+        update (Update): Incoming update containing the message.
+        context (ContextTypes.DEFAULT_TYPE): Contextual information related to the command.
+    """
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="I'm a bot, please talk to me!"
