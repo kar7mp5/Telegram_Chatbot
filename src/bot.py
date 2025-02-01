1e2496d521a025c5402bae44140b11c6a7b36b6b
kar7mp5 <tommy1005a@gmail.com>
2025-02-02 00:28:55 +0900

    fix: Refactoring function naming
    setup_logging --> _setup_Logging
    command_preview --> _command_preview

--- src/bot.py
+++ src/bot.py
@@ -2,13 +2,12 @@
 We are trying to create a bot that can communicate through the LLM model.
 The ability to give commands through `/` in Telegram and chat with bots in general conversations.
 """
-
 import logging
 from telegram import BotCommand
 from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler
 
 # Load bot commands
-import Commands
+from Commands import *
 
 # Load the Telegram bot API key
 from dotenv import load_dotenv
@@ -25,32 +24,10 @@ class Agent(object):
         """
         load_dotenv()
         TOKEN = os.getenv("BOT_TOKEN")
-
         self.application = ApplicationBuilder().token(TOKEN).build()
-        self.setup_logging()
-        self.command_preview()
 
-    def setup_logging(self):
-        """
-        Sets up logging for the application with a specific log format
-        and log level set to INFO.
-        """
-        logging.basicConfig(
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            level=logging.INFO
-        )
-
-    def command_preview(self):
-        """
-        Registers the bot commands that users can call, such as 'help' and 'weather'.
-        These commands provide descriptions for use in the bot interface.
-        """
-        commands_list = [
-            BotCommand(command="help", description="도움말 보기"),
-            BotCommand(command="weather", description="날씨 정보 확인"),
-            BotCommand(command="gpt", description="GPT 질문")
-        ]
-        self.application.bot.set_my_commands(commands_list)
+        self._setup_logging()
+        self._command_preview()
 
     def update_handler(self):
         """
@@ -67,22 +44,44 @@ class Agent(object):
 
         Runs the polling process to listen for incoming messages.
         """
-        start_handler = CommandHandler('start', Commands.start)
+        start_handler = CommandHandler('start', Start)
         self.application.add_handler(start_handler)
 
-        help_handler = CommandHandler('help', Commands.help)
+        help_handler = CommandHandler('help', Help)
         self.application.add_handler(help_handler)
 
-        weather_handler = CommandHandler('weather', Commands.weather)
+        weather_handler = CommandHandler('weather', Weather)
         self.application.add_handler(weather_handler)
 
-        self.application.add_handler(CommandHandler('gpt', Commands.gpt_response))
-        self.application.add_handler(CallbackQueryHandler(Commands.handle_callback_query))
+        self.application.add_handler(CommandHandler('gpt', GPT_response))
+        self.application.add_handler(CallbackQueryHandler(Handle_callback_query))
 
-        unknown_handler = MessageHandler(filters.COMMAND, Commands.unknown)
+        unknown_handler = MessageHandler(filters.COMMAND, Unknown)
         self.application.add_handler(unknown_handler)
 
         self.application.run_polling()
+
+    def _setup_logging(self):
+        """
+        Sets up logging for the application with a specific log format
+        and log level set to INFO.
+        """
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            level=logging.INFO
+        )
+
+    def _command_preview(self):
+        """
+        Registers the bot commands that users can call, such as 'help' and 'weather'.
+        These commands provide descriptions for use in the bot interface.
+        """
+        commands_list = [
+            BotCommand(command="help", description="도움말 보기"),
+            BotCommand(command="weather", description="날씨 정보 확인"),
+            BotCommand(command="gpt", description="GPT 질문")
+        ]
+        self.application.bot.set_my_commands(commands_list)
 
 
 if __name__ == '__main__':
