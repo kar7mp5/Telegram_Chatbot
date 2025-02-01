b63d308d8c4817007f1b64488d345d7dde2d3823
kar7mp5 <tommy1005a@gmail.com>
2025-02-02 00:32:18 +0900

    fix: Fix the name of functions

--- src/Commands/__init__.py
+++ src/Commands/__init__.py
@@ -1,12 +1,10 @@
 from os.path import dirname
 from sys import path
 
-
 path.insert(0, dirname(__file__))
 
-
-from .start import start
-from .help import help
-from .weather import weather
-from .gpt_response import gpt_response, handle_callback_query
-from .unknown import unknown
+from .start import Start
+from .help import Help
+from .weather import Weather
+from .gpt_response import GPT_response, Handle_callback_query
+from .unknown import Unknown
