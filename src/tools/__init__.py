bd09f8f04ddf86de88a305613d688f27da033c0a
kar7mp5 <tommy1005a@gmail.com>
2025-02-07 15:13:49 +0900

    feat(logger): Change the name of file logger_config to logger

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -5,5 +5,5 @@ path.insert(0, dirname(__file__))
 
 from .text2markdown import text2markdown
 from .send_message import send_message
-from .logger_config import setup_logger
+from .logger import setup_logger
 from .load_prompt import load_prompt
